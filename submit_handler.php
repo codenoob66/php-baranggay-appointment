8c5e47eb8a40b2240dbc6dacd1a56ef6fcdd9a69
rafael vincent <d749107@gmail.com>
2025-12-07 17:28:31 +0800

    working on sql

--- submit_handler.php
+++ submit_handler.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "db.php";
+include "util/randomId.php";
 // error_reporting(E_ALL);
 // ini_set('display_errors', 1);
 // Set the content type to ensure the response is treated as plain text or JSON
@@ -9,7 +10,7 @@ header('Content-Type: application/json');
 // Check if the request method is POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the 'name' and 'email' fields are set
-    if (isset($_POST['user-name']) && isset($_POST['appointment-type']) && isset($_POST['appointment-time'])) {
+    if (isset($_POST['first-name']) && isset($_POST['last-name']) && isset($_POST['appointment-type']) && isset($_POST['appointment-time'])) {
         // 1. Sanitize the incoming data (Crucial for security!)
         $first_name = htmlspecialchars($_POST['first-name']);
         $last_name = htmlspecialchars($_POST['last-name']);
@@ -19,15 +20,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // 2. Perform server-side actions (e.g., save to a database, send an email)
 
         // Example: Log the received data
-        $data = ["message" => $name, "appointment" => $appointment_schedule, "Appointment Type" => $appType];
+        // $data = ["message" => $name, "appointment" => $appointment_schedule, "Appointment Type" => $appType];
         $log_message = "New appointment for $first_name $last_name, on $appointment_schedule for $appType\n";
         file_put_contents('submissions.log', $log_message, FILE_APPEND);
         // 3. Send a response back to the JavaScript (AJAX success function)
         $sql = "INSERT INTO clients(Client_FirstName, Client_LastName) VALUES (?,?)";
         $stmt = $conn->prepare($sql);
-        $stmt->execute([$first_name, $last_name]);
+        $stmt->bindParam(1, $first_name);
+        $stmt->bindParam(2, $last_name);
+        $stmt->execute();
         $stmt = null;
-        echo json_encode($data);
+        // echo json_encode($data);
     } else {
         // Handle missing fields error
         http_response_code(400); // Bad Request
@@ -39,5 +42,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Handle non-POST requests
     http_response_code(405); // Method Not Allowed
     echo "Error: Invalid request method.";
-    echo $dbname;
 }
---
+++ util/randomId.php
@@ -0,0 +1,17 @@
+<?php
+
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    die('Direct access not permitted');
+}
+
+
+function generateRandom()
+{
+    $randomStr = '';
+    $characterSet = "1234567890abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLXCVZBNM";
+    for ($i = 0; $i < 10; $i++) {
+        $randomStr .= $characterSet[random_int(0, strlen($characterSet) - 1)];
+    }
+
+    echo $randomStr;
+}
